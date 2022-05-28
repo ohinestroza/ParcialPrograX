d576c80d67bbcffeaee970fdbfc284bd64e0268b
ohinestroza <52361012+ohinestroza@users.noreply.github.com>
2022-05-28 16:42:31 -0600

    Add files via upload

---
+++ Parcial-PrograX/Parcial-PrograX/Interfaces/ICalculos.cs
@@ -0,0 +1,29 @@
+﻿using Parcial_PrograX.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial_PrograX.Interfaces
+{
+    interface iColchonAhorro
+    {
+        double calcularColchonAhorro(Clientes cliente);
+    }
+
+    interface iIVA
+    {
+        double calcularIVA(Clientes cliente);
+    }
+
+    interface iISR
+    {
+        double calcularISR(Clientes cliente);
+    }
+
+    interface IDescuento : iColchonAhorro, iIVA, iISR
+    {
+
+    }
+}
---
+++ Parcial-PrograX/Parcial-PrograX/Interfaces/ICliente.cs
@@ -0,0 +1,29 @@
+﻿using Parcial_PrograX.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial_PrograX.Interfaces
+{
+    interface ICreateClient
+    {
+        void createClient(Clientes cliente);
+    }
+    interface IDeleteClient
+    {
+        void deleteClient(Clientes cliente);
+    }
+
+    interface IUpdateClient
+    {
+        void updateClient(Clientes cliente);
+    }
+
+    interface IAsignacionCliente : ICreateClient, IDeleteClient, IUpdateClient
+    {
+
+    }
+
+}
